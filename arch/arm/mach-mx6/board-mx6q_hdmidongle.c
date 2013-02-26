4968635268fbd6da0980b2b80dbd00a586c1d949
James Laird <jhl@mafipulation.org>
2013-02-26 22:15:43 +1100

    hdmidongle: enable CAAM crypto processor
    Amongst other things, the CAAM provides a fast hardware random number
    generator. This is particularly useful here, as the kernel has few
    entropy sources to draw on in this device otherwise, and random number
    generation (eg. for crypto keys) is ridiculously slow (~8 bytes/30 sec
    type slow).
    
    The CAAM provides both /dev/crypto acceleration interface, and
    /dev/hwrng random number source. To feed the kernel entropy pool,
    install rngd from rng-tools, and then:
    
        rngd -r /dev/hwrng -o /dev/random
    
    and you will never run out of good random numbers again :)

--- arch/arm/mach-mx6/board-mx6q_hdmidongle.c
+++ arch/arm/mach-mx6/board-mx6q_hdmidongle.c
@@ -393,6 +393,8 @@ static void __init mx6_hdmidongle_board_init(void)
 
 	imx6q_add_imx_snvs_rtc();
 
+	imx6q_add_imx_caam();
+
 	imx6q_add_imx_i2c(1, &mx6q_hdmidongle_i2c_data);
 	imx6q_add_imx_i2c(2, &mx6q_hdmidongle_i2c_data);
     
